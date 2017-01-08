4547467755b88bf78f91876d3b59b2f16c4fd090
timoffex <timoffex@gmail.com>
2017-01-07 21:52:24 -0800

    slight reorganization
    Moved “Defaults” folder to “Editor/Defaults” to avoid confusion.

--- Assets/Editor/PlatformCreation.cs
+++ Assets/Editor/PlatformCreation.cs
@@ -8,7 +8,7 @@ public class PlatformCreation {
 	public static void CreatePlatform () {
 
 		// Load the default platform.
-		var defaultPlatform = AssetDatabase.LoadAssetAtPath<GameObject> ("Assets/Defaults/Default Platform.prefab");
+		var defaultPlatform = AssetDatabase.LoadAssetAtPath<GameObject> ("Assets/Editor/Defaults/Default Platform.prefab");
 
 		// Instantiate the default platform.
 		var platform = GameObject.Instantiate (defaultPlatform);
