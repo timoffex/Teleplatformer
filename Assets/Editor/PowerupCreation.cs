08b4a565e5451f9b4b37a053e9fc582732e45ddc
timoffex <timoffex@gmail.com>
2017-01-12 19:32:28 -0800

    Powerup Zones
    Powerup Zones are placed in chunks to generate random power-ups from a
    list.

--- Assets/Editor/PowerupCreation.cs
+++ Assets/Editor/PowerupCreation.cs
@@ -28,4 +28,27 @@ public class PowerupCreation {
 		// Ping it as a visual cue. -- this part doesn't work for some reason
 		EditorGUIUtility.PingObject (powerup);
 	}
+
+	[MenuItem ("GameObject/Create Other/Powerup Zone")]
+	public static void CreatePowerupZone () {
+
+		// Load the default powerup zone.
+		var defaultPowerupZone = AssetDatabase.LoadAssetAtPath<GameObject> ("Assets/Editor/Defaults/Default Powerup Zone.prefab");
+
+		// Instantiate the default powerup zone.
+		var powerupZone = GameObject.Instantiate (defaultPowerupZone);
+		powerupZone.name = "New Powerup Zone";
+
+
+		// If an object is selected, add the powerup zone as its child.
+		var selection = Selection.activeGameObject;
+		if (selection != null)
+			powerupZone.transform.parent = selection.transform;
+
+		// Position the powerup zone at the center of the screen.
+		powerupZone.transform.position = (Vector2)SceneView.lastActiveSceneView.camera.transform.position;
+
+		// Ping it as a visual cue. -- this part doesn't always work for some reason
+		EditorGUIUtility.PingObject (powerupZone);
+	}
 }
